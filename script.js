44bfa77f40e67af26084284d661dc16df9d1615b
hasnain-shirsho-csl <bsse1106@iit.du.ac.bd>
2023-05-17 13:57:44 +0600

    starting to take shape

--- script.js
+++ script.js
@@ -1,14 +1,22 @@
-// const nav = document.querySelector('header');
-// window.addEventListener('scroll', () => {
-//   if (window.scrollY > 0) {
-//     nav.classList.add('dark');
-//     console.log("dark");
-//   } else {
-//     nav.classList.remove('dark');
-//     console.log("no dark dark");
-//   }
-// });
+window.addEventListener('scroll', function() {
+  const header = document.querySelector('header');
+  if (window.scrollY > 0) {
+    header.classList.add('header-scroll');
+  } else {
+    header.classList.remove('header-scroll');
+  }
+});
 
+const carouselImages = document.querySelectorAll('.carousel-image');
+let currentImageIndex = 0;
+
+function showNextImage() {
+  carouselImages[currentImageIndex].classList.remove('active');
+  currentImageIndex = (currentImageIndex + 1) % carouselImages.length;
+  carouselImages[currentImageIndex].classList.add('active');
+}
+
+setInterval(showNextImage, 3000);
 
 const navSlide = () => {
   const burger = document.querySelector('.burger');
